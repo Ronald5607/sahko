49fe8d12a7dbc07e24d5d3ca110a837222b05179
Ronald5607 <roni.paloniemi@hotmail.com>
2023-04-16 21:31:35 +0300

    added hover effects

--- src/Components/Columns.tsx
+++ src/Components/Columns.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../Styles/Columns.css';
 
 interface ColumnProps {
@@ -6,13 +7,21 @@ interface ColumnProps {
 };
 
 export default function Columns(Props: ColumnProps) {
-    
+
+    const [ShowPrice, setShowPrice] = useState(-1);
+
     const columnArray = Props.PriceArray.map((column, index) => {
         const height = (column / Props.MaxPrice) * 100 + '%';
             return (<div
-                className='column' key={index}
-                style={{ height: height }}
-            ></div>);
+                className='column'
+                key={index} style={{ height: height }}
+                onMouseEnter={() => {setShowPrice(index)}}
+                onMouseLeave={() => {setShowPrice(-1)}}
+                >
+                    <div className='price' style={{display: index === ShowPrice ? 'block':'none'}}>
+                        {Math.round(column * 10) / 10}
+                    </div>
+            </div>);
         });
     return <>{columnArray}</>
 }
